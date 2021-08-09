40febd79f6bedd515659646206d0dbdf7fce426d
lucorrea30 <correa.lucas30@hotmail.com>
2021-08-08 21:52:30 -0300

    primeira pagina

---
+++ pages/index.js
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+
+function Home() {
+    return ( <div>
+        <h2> Home </h2>
+        <Counter />
+        </div>
+    )
+}
+
+function Counter() {
+    const [counter, setCounter] = useState(1);
+
+    function addCounter() {
+        setCounter(counter +1);
+    }
+
+    return (
+        <div>
+            <div>{counter}</div>
+            <button onClick={addCounter}>Adicionar</button>
+        </div>)
+}
+
+export default Home
